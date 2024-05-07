65e59f53bc875fb2b014e206a649f1c70debe4a1
Mawada Abdurazaq Altkbali <76155745+M-altkbali@users.noreply.github.com>
2024-05-07 12:58:16 +0200

    V1

---
+++ header.php
@@ -0,0 +1,13 @@
+<nav>
+    <div class="logo">
+        <span class="any">Any</span><span class="gym">GYM</span><img src="IMAGE/aboutus/dumbbell_black.png" alt="GYM_Logo" width="40px" height="50px">
+    </div>
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="aboutus.html">About</a></li>
+        <li><a href="#">Courses</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="contact.html">Contact</a></li>
+        <li><a href="#">Shop</a></li>
+    </ul>
+</nav>
